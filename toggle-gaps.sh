d2e3d42348e1e0b6789a854f646edf393791bcc7
Thomas Becker <thomas.becker00@gmail.com>
2024-06-16 11:22:38 +0200

    chore(skhd): increase gaps in menuless mode

--- toggle-gaps.sh
+++ toggle-gaps.sh
@@ -3,7 +3,7 @@ borderFile=/tmp/borders
 if [[ -f /tmp/borders ]]; then
 	# borders width=0
 	yabai -m config external_bar main:0:0
-	yabai -m space --padding abs:16:16:16:16 --gap abs:16
+	yabai -m space --padding abs:32:32:32:32 --gap abs:32
 	rm $borderFile
 else
 	# borders width=4
